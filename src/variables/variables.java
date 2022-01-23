9afa33150c8f19f7869f08273db60d9c22924631
utkarsh <uk556834@gmaqil.com>
2022-01-23 17:24:54 +0530

    variable

---
+++ src/variables/variables.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class variables {
+    public static void main(String[] args) {
+        //syntax- DataTypes variable_name=value;
+
+        String name="utkarsh yadav";
+
+        String city="Agra";
+
+        int  age=25;
+
+        double salary=34500.10;
+
+     System.out.println("name- " + name);
+     System.out.println("city- " +city);
+     System.out.println("age-" +age);
+     System.out.println("salary-" +salary);
+    }
+}
